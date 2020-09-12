3388e21c2b8f9c9385e70d42870a997708b81237
yun-cheese <yun.cheese@kakaocorp.com>
2020-09-12 18:20:45 +0900

    commit

--- src/main/kotlin/com/spring/sample/member/MemberApi.kt
+++ src/main/kotlin/com/spring/sample/member/MemberApi.kt
@@ -6,7 +6,12 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
 import org.springframework.data.web.PageableDefault
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
 import javax.validation.constraints.Email
 
@@ -15,10 +20,7 @@ import javax.validation.constraints.Email
 class MemberApi(
     private val memberRepository: MemberRepository
 ) {
-
     private val log by logger()
-    private val members = mutableListOf<Member>()
-    private var memberId = 1L
 
     @GetMapping("/{id}")
     fun getMember(@PathVariable id: Long): Member {
