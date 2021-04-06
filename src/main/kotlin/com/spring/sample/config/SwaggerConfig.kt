7f332113e46e954036441645e9865fee5c93eed0
cheese10yun <cheese10yun@gmail.com>
2021-04-06 23:59:57 +0900

    불필요 코드 제거

--- src/main/kotlin/com/spring/sample/config/SwaggerConfig.kt
+++ src/main/kotlin/com/spring/sample/config/SwaggerConfig.kt
@@ -1,18 +1,19 @@
 package com.spring.sample.config
 
+import java.time.YearMonth
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
 import springfox.documentation.service.AuthorizationScope
+import springfox.documentation.service.BasicAuth
 import springfox.documentation.service.SecurityReference
+import springfox.documentation.service.SecurityScheme
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
-import java.time.YearMonth
-import springfox.documentation.service.BasicAuth
 
-import springfox.documentation.service.SecurityScheme@Configuration
+@Configuration
 
 @EnableSwagger2
 class SwaggerConfig {
@@ -31,13 +32,6 @@ class SwaggerConfig {
             .select()
             .apis(RequestHandlerSelectors.any())
             .paths(PathSelectors.ant("/api/**"))
-            .build()
-    }
-
-    private fun actuatorSecurityContext(): SecurityContext {
-        return SecurityContext.builder()
-            .securityReferences(Arrays.asList(basicAuthReference()))
-            .forPaths(PathSelectors.ant("/actuator/**"))
             .build()
     }
 
